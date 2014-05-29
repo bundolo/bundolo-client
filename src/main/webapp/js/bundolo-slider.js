cb8ca020ef98946bcb0acc96fcbfb79b24ef99fe
daniel <daniel@frulica>
2014-05-29 17:08:09 +0200

    Slides of contests, users and texts

--- src/main/webapp/js/bundolo-slider.js
+++ src/main/webapp/js/bundolo-slider.js
@@ -11,8 +11,8 @@ function displaySlider() {
 			    { "title": "Serials", "id" : "serials", "icon" : "book" },
 			    { "title": "Authors", "id" : "authors", "icon" : "user" },
 			    { "title": "Announcements", "id" : "announcements", "icon" : "bullhorn", "slides" : [{"content" : "Anemonefish murray cod", "index" : "0"}, {"content" : "Anemonefish murray cod", "index" : "1", "active_slide" : true}, {"content" : "Anemonefish murray cod", "index" : "2"}, {"content" : "Anemonefish murray cod", "index" : "3"} ] },
-			    { "title": "Forum", "id" : "forum", "icon" : "comments-o", "slides" : [{"content" : "Anemonefish murray cod", "index" : "0"}, {"content" : "Anemonefish murray cod", "index" : "1", "active_slide" : true}, {"content" : "Anemonefish murray cod", "index" : "2"}, {"content" : "Anemonefish murray cod", "index" : "3"} ] },
-			    { "title": "Contests", "id" : "contests", "icon" : "eye", "slides" : [{"content" : "Anemonefish murray cod", "index" : "0"}, {"content" : "Anemonefish murray cod", "index" : "1", "active_slide" : true}, {"content" : "Anemonefish murray cod", "index" : "2"}, {"content" : "Anemonefish murray cod", "index" : "3"} ] },
+			    { "title": "Forum", "id" : "topics", "icon" : "comments-o", "slides" : [{"content" : "Anemonefish murray cod", "index" : "0"}, {"content" : "Anemonefish murray cod", "index" : "1", "active_slide" : true}, {"content" : "Anemonefish murray cod", "index" : "2"}, {"content" : "Anemonefish murray cod", "index" : "3"} ] },
+			    { "title": "Contests", "id" : "contests", "icon" : "eye" },
 			    { "title": "Connections", "id" : "connections", "icon" : "link" },
 			  ]
 		});
@@ -22,12 +22,12 @@ function displaySlider() {
 }
 
 function displaySlide(id) {
-	$.getJSON( "http://localhost/connections", { "start": "0", "end": "4", "orderBy": "date,desc", "filterBy": ""}, function( data ) {
-		  $.get('templates/slide.html', function(template) {
+	$.getJSON( "http://localhost/"+id, { "start": "0", "end": "4", "orderBy": "date,desc", "filterBy": ""}, function( data ) {
+		  $.get("templates/slide_"+id+".html", function(template) {
 			  for (index in data) {
 				  data[index].index = index;
 				  if (index == 0) {
-					  data[index].active_slide = true; 
+					  data[index].active_slide = true;
 				  }
 			  }
 			  var rendered = Mustache.render(template, { "id": id, "slides": data });
