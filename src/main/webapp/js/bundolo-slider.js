00f2a35591515d2dcba9fb04d5ae49eda6450f61
daniel <daniel@192.168.1.21>
2014-05-29 02:35:52 +0200

    Slider reading connections

--- src/main/webapp/js/bundolo-slider.js
+++ src/main/webapp/js/bundolo-slider.js
@@ -4,22 +4,34 @@ $(document).ready(function() {
 
 function displaySlider() {
 	$.get('templates/slider.html', function(template) {
-		$.getJSON( "http://localhost/latest", function( data ) {
-			  alert(data);
-			});
 	    var rendered = Mustache.render(template, {
-			  "tabs": [
-			 			    { "title": "Overview", "id" : "overview", "icon" : "flag", "active": true, "slides" : [{"content" : "Anemonefish murray cod", "index" : "0"}, {"content" : "Anemonefish murray cod", "index" : "1", "active_slide" : true}, {"content" : "Anemonefish murray cod", "index" : "2"}, {"content" : "Anemonefish murray cod", "index" : "3"} ]},
-			 			    { "title": "Texts", "id" : "texts", "icon" : "file-text-o", "slides" : [{"content" : "Anemonefish murray cod", "index" : "0"}, {"content" : "Anemonefish murray cod", "index" : "1", "active_slide" : true}, {"content" : "Anemonefish murray cod", "index" : "2"}, {"content" : "Anemonefish murray cod", "index" : "3"} ] },
-			 			    { "title": "Serials", "id" : "serials", "icon" : "book", "slides" : [{"content" : "Anemonefish murray cod", "index" : "0"}, {"content" : "Anemonefish murray cod", "index" : "1", "active_slide" : true}, {"content" : "Anemonefish murray cod", "index" : "2"}, {"content" : "Anemonefish murray cod", "index" : "3"} ] },
-			 			    { "title": "Authors", "id" : "authors", "icon" : "user", "slides" : [{"content" : "Anemonefish murray cod", "index" : "0"}, {"content" : "Anemonefish murray cod", "index" : "1", "active_slide" : true}, {"content" : "Anemonefish murray cod", "index" : "2"}, {"content" : "Anemonefish murray cod", "index" : "3"} ] },
-			 			    { "title": "News", "id" : "news", "icon" : "bullhorn", "slides" : [{"content" : "Anemonefish murray cod", "index" : "0"}, {"content" : "Anemonefish murray cod", "index" : "1", "active_slide" : true}, {"content" : "Anemonefish murray cod", "index" : "2"}, {"content" : "Anemonefish murray cod", "index" : "3"} ] },
-			 			    { "title": "Forum", "id" : "forum", "icon" : "comments-o", "slides" : [{"content" : "Anemonefish murray cod", "index" : "0"}, {"content" : "Anemonefish murray cod", "index" : "1", "active_slide" : true}, {"content" : "Anemonefish murray cod", "index" : "2"}, {"content" : "Anemonefish murray cod", "index" : "3"} ] },
-			 			    { "title": "Contests", "id" : "contests", "icon" : "eye", "slides" : [{"content" : "Anemonefish murray cod", "index" : "0"}, {"content" : "Anemonefish murray cod", "index" : "1", "active_slide" : true}, {"content" : "Anemonefish murray cod", "index" : "2"}, {"content" : "Anemonefish murray cod", "index" : "3"} ] },
-			 			    { "title": "Connections", "id" : "connections", "icon" : "link", "slides" : [{"content" : "Anemonefish murray cod", "index" : "0"}, {"content" : "Anemonefish murray cod", "index" : "1", "active_slide" : true}, {"content" : "Anemonefish murray cod", "index" : "2"}, {"content" : "Anemonefish murray cod", "index" : "3"} ] },
-			 			  ]
-			 			});
+	    	"tabs": [
+			    { "title": "Overview", "id" : "overview", "icon" : "flag", "active": true },
+			    { "title": "Texts", "id" : "texts", "icon" : "file-text-o" },
+			    { "title": "Serials", "id" : "serials", "icon" : "book" },
+			    { "title": "Authors", "id" : "authors", "icon" : "user" },
+			    { "title": "Announcements", "id" : "announcements", "icon" : "bullhorn", "slides" : [{"content" : "Anemonefish murray cod", "index" : "0"}, {"content" : "Anemonefish murray cod", "index" : "1", "active_slide" : true}, {"content" : "Anemonefish murray cod", "index" : "2"}, {"content" : "Anemonefish murray cod", "index" : "3"} ] },
+			    { "title": "Forum", "id" : "forum", "icon" : "comments-o", "slides" : [{"content" : "Anemonefish murray cod", "index" : "0"}, {"content" : "Anemonefish murray cod", "index" : "1", "active_slide" : true}, {"content" : "Anemonefish murray cod", "index" : "2"}, {"content" : "Anemonefish murray cod", "index" : "3"} ] },
+			    { "title": "Contests", "id" : "contests", "icon" : "eye", "slides" : [{"content" : "Anemonefish murray cod", "index" : "0"}, {"content" : "Anemonefish murray cod", "index" : "1", "active_slide" : true}, {"content" : "Anemonefish murray cod", "index" : "2"}, {"content" : "Anemonefish murray cod", "index" : "3"} ] },
+			    { "title": "Connections", "id" : "connections", "icon" : "link" },
+			  ]
+		});
 	    $(".slider").html(rendered);
 	    //TODO assign event handlers if any
 	  });
+}
+
+function displaySlide(id) {
+	$.getJSON( "http://localhost/connections", { "start": "0", "end": "4", "orderBy": "date,desc", "filterBy": ""}, function( data ) {
+		  $.get('templates/slide.html', function(template) {
+			  for (index in data) {
+				  data[index].index = index;
+				  if (index == 0) {
+					  data[index].active_slide = true; 
+				  }
+			  }
+			  var rendered = Mustache.render(template, { "id": id, "slides": data });
+			  $(".slider #"+id+"-carousel>div").html(rendered);
+		  });
+	});
 }
