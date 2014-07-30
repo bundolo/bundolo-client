d8cd71a7fb48008e61cd915cf08823a7c22a4f4e
daniel-farkas <daniel.farkas@codecentric.de>
2014-07-30 04:53:18 +0200

    Sorting in sidebar

--- src/main/webapp/js/bundolo-sidebar.js
+++ src/main/webapp/js/bundolo-sidebar.js
@@ -1,10 +1,25 @@
 $(document).ready(function() {
 	displaySidebar();
+	//clicks on entries in tables
 	$('body').on('click', '#sidebarAccordion>li .panel-collapse table>tbody>tr', function(e) {
 		var elementId = $(this).attr('id');
 		var itemType = elementId.substr(0, elementId.indexOf('_'));
 		var itemId = elementId.substr(itemType.length + 1);
 		$.address.value(rootFolder+itemType+"/" + itemId);
+	});
+	//clicks on column headers
+	$('body').on('click', '#sidebarAccordion>li .panel-collapse table>thead>tr>th', function(e) {
+		var elementId = $(this).attr('id');
+		var itemType = elementId.substr(0, elementId.indexOf('_'));
+		var columnName = elementId.substr(itemType.length + 8);
+		if (($(this).hasClass("asc")) || ($(this).hasClass("desc")))  {
+			$(this).toggleClass("asc desc");
+		} else {
+			$('#sidebarAccordion>li #collapse_'+itemType+' table>thead>tr>th').removeClass("asc desc");
+			$(this).toggleClass("asc");
+		}
+		//TODO filtering
+		displaySidebarAccordion(itemType, columnName,  $(this).attr('class'), '', '');
 	});
 });
 
@@ -19,13 +34,13 @@ function displaySidebar() {
 	$.get('/templates/sidebar.html', function(template) {
 	    var rendered = Mustache.render(template, {
 			  "collapsibles": [
-			 			    { "title": "Texts", "id" : "texts", "icon" : "file-text-o", "columns" : ["author", "title", "date", "last_activity"] },
-			 			    { "title": "Serials", "id" : "serials", "icon" : "book", "columns" : ["author", "title", "date", "last_activity"] },
-			 			    { "title": "Authors", "id" : "authors", "icon" : "user", "columns" : ["author", "title", "date", "last_activity"] },
-			 			    { "title": "Announcements", "id" : "announcements", "icon" : "bullhorn", "columns" : ["author", "title", "date", "last_activity"] },
-			 			    { "title": "Forum", "id" : "topics", "icon" : "comments-o", "columns" : ["author", "title", "date", "last_activity"], "categories" : [{"title" : "Literature", "id" : "literature"}, {"title" : "Bundolo", "id" : "bundolo"}, {"title" : "Various", "id" : "various"}, {"title" : "Suggestions", "id" : "suggestions"}, {"title" : "Archive", "id" : "archive"} ] },
-			 			    { "title": "Contests", "id" : "contests", "icon" : "eye", "columns" : ["author", "title", "date", "last_activity"] },
-			 			    { "title": "Connections", "id" : "connections", "icon" : "link", "columns" : ["author", "title", "date", "last_activity"], "categories" : [{"title" : "Literature", "id" : "literature"}, {"title" : "Art", "id" : "art"}, {"title" : "Alternative comics", "id" : "comics"}, {"title" : "Online magazines", "id" : "magazines"}, {"title" : "Underground", "id" : "underground"} ] },
+			 			    { "title": "tekstovi", "id" : "texts", "icon" : "file-text-o", "columns" : [{"column_title" : "naslov", "column_name" : "title"}, {"column_title" : "autor", "column_name" : "author"}, {"column_title" : "objavljivanje", "column_name" : "date", "column_order" : "desc"}, {"column_title" : "aktivnost", "column_name" : "last_activity"}] },
+			 			    { "title": "serije", "id" : "serials", "icon" : "book", "columns" : [{"column_title" : "naslov", "column_name" : "title"}, {"column_title" : "autor", "column_name" : "author"}, {"column_title" : "objavljivanje", "column_name" : "date", "column_order" : "desc"}, {"column_title" : "aktivnost", "column_name" : "last_activity"}] },
+			 			    { "title": "autori", "id" : "authors", "icon" : "user", "columns" : [{"column_title" : "korisničko ime", "column_name" : "author"}, {"column_title" : "opis", "column_name" : "description"}, {"column_title" : "registracija", "column_name" : "date", "column_order" : "desc"}, {"column_title" : "aktivnost", "column_name" : "last_activity"}] },
+			 			    { "title": "vesti", "id" : "announcements", "icon" : "bullhorn", "columns" : [{"column_title" : "naslov", "column_name" : "title"}, {"column_title" : "autor", "column_name" : "author"}, {"column_title" : "objavljivanje", "column_name" : "date", "column_order" : "desc"}, {"column_title" : "aktivnost", "column_name" : "last_activity"}] },
+			 			    { "title": "diskusije", "id" : "topics", "icon" : "comments-o", "columns" : [{"column_title" : "naslov", "column_name" : "title"}, {"column_title" : "autor", "column_name" : "author"}, {"column_title" : "objavljivanje", "column_name" : "date", "column_order" : "desc"}, {"column_title" : "aktivnost", "column_name" : "last_activity"}], "categories" : [{"title" : "književnost", "id" : "literature"}, {"title" : "bundolo", "id" : "bundolo"}, {"title" : "razno", "id" : "various"}, {"title" : "predlozi", "id" : "suggestions"}, {"title" : "arhiva", "id" : "archive"} ] },
+			 			    { "title": "konkursi", "id" : "contests", "icon" : "eye", "columns" : [{"column_title" : "naslov", "column_name" : "title"}, {"column_title" : "autor", "column_name" : "author"}, {"column_title" : "objavljivanje", "column_name" : "date", "column_order" : "desc"}, {"column_title" : "aktivnost", "column_name" : "last_activity"}] },
+			 			    { "title": "linkovi", "id" : "connections", "icon" : "link", "columns" : [{"column_title" : "naslov", "column_name" : "title"}, {"column_title" : "autor", "column_name" : "author"}, {"column_title" : "objavljivanje", "column_name" : "date", "column_order" : "desc"}, {"column_title" : "aktivnost", "column_name" : "last_activity"}], "categories" : [{"title" : "književnost", "id" : "literature"}, {"title" : "kultura", "id" : "art"}, {"title" : "alternativni strip", "id" : "comics"}, {"title" : "online magazini", "id" : "magazines"}, {"title" : "alternativna kultura", "id" : "underground"} ] },
 			 			  ]
 			 			});
 	    $(".sidebar").html(rendered);
@@ -59,14 +74,24 @@ function displaySidebar() {
 	  });
 }
 
-function displaySidebarAccordion(type) {
+function displaySidebarAccordion(type, orderBy, order, filterBy, filter) {
+	order = typeof order !== 'undefined' ? order : 'desc';
+	if (typeof orderBy === 'undefined') {
+		orderBy = 'date';
+		$('#sidebarAccordion>li #collapse_'+type+' table>thead>tr>th').removeClass("asc desc");
+		$('#sidebarAccordion>li #collapse_'+type+' table>thead>tr>#'+type+'_column_date').addClass(order);
+	}	
+	filterBy = typeof filterBy !== 'undefined' ? filterBy : '';
+	filter = typeof filter !== 'undefined' ? filter : '';
+	var orderByParam = orderBy + ',' + order;
+	var filterByParam = filterBy != '' ? filterBy + ',' + filter : '';
 	var table = $('.sidebar #collapse_'+type+' table');
 	table.addClass('hide');
 	var itemCounter = 0;
 	var itemInitial = 25;
 	var itemAdditional = 10;
 	$.get('/templates/sidebar_'+type+'.html', function(template) {
-		$.getJSON(rootPath + restRoot + "/" + type, { "start": itemCounter, "end": (itemCounter + itemInitial -1), "orderBy": "date,desc", "filterBy": ""}, function( data ) {
+		$.getJSON(rootPath + restRoot + "/" + type, { "start": itemCounter, "end": (itemCounter + itemInitial -1), "orderBy": orderByParam, "filterBy": filterByParam}, function( data ) {
 			var escapeUrl = function () {
 				return function(val, render) {
 				    return render(val).replace(/ /g, '~');
