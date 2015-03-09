568f80e2ec7fc0d1eeff9be1fbda14e3620d5a4d
daniel-farkas <daniel.farkas@codecentric.de>
2015-03-09 02:00:17 +0100

    fixed comments

--- src/main/webapp/js/bundolo-context-1.0.8.js
+++ src/main/webapp/js/bundolo-context-1.0.8.js
@@ -17,23 +17,26 @@ $(document).ready(function() {
 	$(document).mouseup(function (e) {
 		var modalDialog = $('#modal');
 		var contextMenu = $('.context-menu');
-	
+
 	    if (!modalDialog.is(e.target) && modalDialog.has(e.target).length === 0 && !contextMenu.is(e.target) && contextMenu.has(e.target).length === 0) {
 	        if(contextMenu.is(":visible")) {
             	contextMenu.hide();
             }
 	    }
 	});
-	
+
 	$('body').on('click', '.save_comment', function(e) {
 		if (!handlingForm) {
 			handlingForm = true;
 			saveComment();
 		}
 	});
-	
+
 	$('body').on('click', '.root-comment-button', function(e) {
-		var parentElement = $('.context-menu>div>div');
+		var parentElement = $('.context-root');
+		if (!parentElement.length) {
+			parentElement = $('.context-menu>div>div');
+		}
 		var parentId = parentElement.find(">span").attr('id').substr(8);
 		addComment(parentElement, parentId);
 	});
@@ -42,7 +45,7 @@ $(document).ready(function() {
 		var parentId = parentElement.find(">span").attr('id').substr(8);
 		addComment(parentElement.find('>.panel-collapse>.panel-body>.panel-group>div>div'), parentId);
 	});
-	
+
 	$('body').on('mouseenter', '.comments-button, .root-comment-button, .comment-button, .rating-select, .item-list-select', function(e) {
 		$(this).parent().addClass("hover");
 		if ($(this).parent().parent().css("overflow")=="hidden") {
@@ -83,7 +86,7 @@ function setContextMenuPostion(event) {
     var menuPosition = {};
     var menuDimension = {};
     menuDimension.x = contextMenu.outerWidth();
-    menuDimension.y = contextMenu.outerHeight();    
+    menuDimension.y = contextMenu.outerHeight();
     mousePosition.x = event.pageX;
     mousePosition.y = event.pageY;
     if (mousePosition.x + menuDimension.x > $(window).width() + $(window).scrollLeft()) {
@@ -131,7 +134,7 @@ function addContextMenu(parentElement, parentId, parentType) {
 				}
 			}
 		});
-		
+
 		$.ajax({
 		    url: rootPath + restRoot + "/rating/" + parentId,
 		    type: 'GET',
@@ -161,29 +164,52 @@ function addContextMenu(parentElement, parentId, parentType) {
 	}
 
 	rootType = parentType;
-	var commentsButton = $('<span class="fa-stack fa-2x comments-button" id="comments_' + parentId + '">\
-			<i class="fa fa-circle fa-stack-2x"></i>\
-			<i class="fa fa-comment-o fa-stack-1x fa-inverse"></i>\
+
+	$('.context-root').addClass('fixed');
+	var toggleComments = $('<span class="btn btn-default pull-right toggle-comments" id="comments_' + parentId + '">\
+			<i class="fa fa-angle-double-up"></i>\
 			</span>');
-	commentsButton.click(function(e) {
-		rootParentId = parentId;
-		displayComments(rootParentId);
-		setContextMenuPostion(e);
-        return false;
-    });
-	commentsButton.attr("title", "komentari");
-	parentElement.append(commentsButton);
+	toggleComments.click(function(e) {
+		toggleComments.toggleClass('detached');
+		var contextRoot = $('.context-root.fixed');
+		if (contextRoot.length) {
+			toggleComments.find('i').toggleClass('fa-angle-double-up fa-angle-double-down');
+			contextRoot.removeClass('fixed');
+			var commentsButton = $('<span class="fa-stack fa-2x comments-button" id="comments_' + parentId + '">\
+					<i class="fa fa-circle fa-stack-2x"></i>\
+					<i class="fa fa-comment-o fa-stack-1x fa-inverse"></i>\
+					</span>');
+			commentsButton.click(function(e) {
+				rootParentId = parentId;
+				displayComments(rootParentId);
+				setContextMenuPostion(e);
+		        return false;
+		    });
+			commentsButton.attr("title", "komentari");
+			parentElement.append(commentsButton);
+		} else {
+			$('.comments-button').remove();
+			$('.context-root').addClass('fixed');
+			displayComments(rootParentId);
+		}
+	});
+	parentElement.append(toggleComments);
+	rootParentId = parentId;
+	displayComments(rootParentId);
 }
 
 function displayComments(parentId) {
-	var contextRootElement = $('.context-menu>div>div');
+	var contextRootElement = $('.context-root.fixed');
+	if (!contextRootElement.length) {
+		contextRootElement = $('.context-menu>div>div');
+	}
 	contextRootElement.html(spinner);
 	$.get(rootFolder+"templates/comments" + "-" + version + ".html", function(template) {
 		$.getJSON(rootPath + restRoot + "/parent_comments/" + parentId, function(data) {
 			sanitizeRecursive(data);
 			var partials = {commentPanel: template};
 		    var rendered = Mustache.render(template, {"comments": data}, partials);
-		    var rootCommentButton = $('<span title="dodaj komentar" class="fa-stack fa-2x pull-right root-comment-button" id="comment_'+parentId+'">\
+		    var rootCommentButton = $('<h4>komentari</h4><span title="dodaj komentar" class="fa-stack fa-2x pull-right root-comment-button" id="comment_'+parentId+'">\
 					<i class="fa fa-circle fa-stack-2x"></i>\
 					<i class="fa fa-plus fa-stack-1x fa-inverse"></i>\
 				</span>');
@@ -334,11 +360,11 @@ function refreshItemListDefault() {
 	if (itemLists && itemLists.length > 0) {
 		if (itemListDefault.is(':selected')) {
 			itemListDefault.text("dodaj u izbor");
-		} else {		
+		} else {
 			itemListDefault.text("izbaci iz izbora");
 		}
 	} else {
 		itemListDefault.text("kreirajte novi izbor da biste mogli dodavati u njega");
 	}
-	
+
 }
